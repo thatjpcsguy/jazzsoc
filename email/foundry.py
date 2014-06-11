514924dd38af72347a672c16b15caf96c6fb6b91
James Cooper-Stanbury <thatguy@jpcs.me>
2014-06-11 22:01:07 +1000

    end of semester

--- email/foundry.py
+++ email/foundry.py
@@ -20,7 +20,7 @@ html = """\
             <img style="width: 600px; display: block; margin: 0 auto;" src="http://jazzsoc.org/img/email_header.jpg" />
         </a>
         <div style="margin: 0 auto; background-color: white; width:550px; padding:25px;">
-            <p>""" + first_name + """, we have one hell of a week planned!</p>
+            <p>""" + first_name + """, thanks for a great semester 1!</p>
 
             <h4>2014 Swing Ball</h4>
             <p>Thankyou to everyone who came to our first ever swing ball! For those who came I think you will agree it was a totally awesome night and I for sure am looking forward to next years ball. <a href="https://www.facebook.com/media/set/?set=a.785602578141368.1073741844.693427937358833&type=1">Here is a link</a> to the all the photos on facebook. Thanks to <a href="https://www.facebook.com/houseofcameo">House of Cameo</a> for the great shots!</p>
@@ -29,7 +29,7 @@ html = """\
             <p>On behalf of the JazzSoc team, I would like to wish you luck with your exams and final assessments. We'll see you back next semester with a ton of cool stuff!</p>
 	
             <br /><br /><br />
-            <p>Cheers,</p><br />
+            <p>Cheers,</p>
             <p>James Peter Cooper-Stanbury<br />Jazz Society President</p>
         </div>
         <div style="width: 600px; padding-top: 50px; margin: 0 auto; height: 100px; background-color: #2c3e50; text-align: center;">
