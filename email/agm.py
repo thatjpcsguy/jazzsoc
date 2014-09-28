17897aa4f2b8021ae39caecfc33a37d644038c66
James Cooper-Stanbury <thatguy@jpcs.me>
2014-09-28 11:18:50 +1000

    added auth

--- email/agm.py
+++ email/agm.py
@@ -22,17 +22,17 @@ html = """\
         <div style="margin: 0 auto; background-color: white; width:550px; padding:25px;">
             <p>""" + first_name + """, get ready for 2015!</p>
 
-            <h2>AGM - 10th September
-            <p>A reminder that Jazzsoc’s Annual General Meeting (AGM) is coming up on the 10th of September at 7pm at the Old Darlington School (near engineering). It is at this meeting where we will say farewell to our old executive team and vote in some new executive members for 2015. THERE WILL BE PIZZA!! YAY!
-            All members are encouraged to attend the AGM, and anyone can nominate themselves for an executive position if interested. Being on the executive team means that you’ll be involved with running the society. If you have ideas for events or programs that you’d like to see put into action, this is your opportunity to take the initiative and help us make it happen for you. JazzSoc has potential to be a thriving society, with weekly jazz events and social activities – but we need YOU to help us make that happen.
-            As we have a large number of executives leaving university at the end of this year, we would really love to encourage as many people to apply for this as possible. It doesn’t matter if you’re new to the uni or haven’t been to many of our events – don’t be shy! Getting on the exec is the best way to meet new people and be right in the thick of it!
-            Key executive positions include:
-            President, Vice-President, Treasurer, Secretary
-            Additional executive positions include:
-            Big Band Manager, Events Manager, Publicity Manager
-            There are also additional positions for general executive members (without specific titles), who will be expected to help out with general tasks when needed. All executive members will be able to suggest and organise events or programs to carry out throughout the year.
+            <h2>AGM - 10th September</h2>
+            <p>A reminder that Jazzsoc's Annual General Meeting (AGM) is coming up on the 10th of September at 7pm at the Old Darlington School (near engineering). It is at this meeting where we will say farewell to our old executive team and vote in some new executive members for 2015. THERE WILL BE PIZZA!! YAY!</p>
+            <p>All members are encouraged to attend the AGM, and anyone can nominate themselves for an executive position if interested. Being on the executive team means that you'll be involved with running the society. If you have ideas for events or programs that you'd like to see put into action, this is your opportunity to take the initiative and help us make it happen for you. JazzSoc has potential to be a thriving society, with weekly jazz events and social activities - but we need YOU to help us make that happen.</p>
+            <p>As we have a large number of executives leaving university at the end of this year, we would really love to encourage as many people to apply for this as possible. It doesn't matter if you're new to the uni or haven't been to many of our events - don't be shy! Getting on the exec is the best way to meet new people and be right in the thick of it!</p>
+            <p>Key executive positions include:</p>
+            <p>President, Vice-President, Treasurer, Secretary</p>
+            <p>Additional executive positions include:</p>
+            <p>Big Band Manager, Events Manager, Publicity Manager, Band Bookings Manager</p>
+            <p>There are also additional positions for general executive members (without specific titles), who will be expected to help out with general tasks when needed. All executive members will be able to suggest and organise events or programs to carry out throughout the year.</p>
             There can be multiple people nominated for each role, and an individual person can nominate themselves for multiple roles. During the meeting, each nominee will be given the chance to give a short (informal) speech giving their reasons as to why they should be elected. All members at the meeting will then vote to select who will go on the 2015 team.
-            If you are interested in nominating yourself or finding out more information, don’t hesitate to contact us at contact@jazzsoc.org. Otherwise, we will see you on the 10th of September!
+            If you are interested in nominating yourself or finding out more information, don't hesitate to contact us at contact@jazzsoc.org. Otherwise, we will see you on the 10th of September!
             </p>
 
             <h2>Science Revue 2014</h2>
